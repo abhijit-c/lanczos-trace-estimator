200c6bfe9267c47c352caae89dbb0bd180c41723
abhijit-c <abhijit9331@gmail.com>
2023-10-29 13:57:06 -0400

    Actually, is hutchinson terrible?

--- src/tests/test_trace_estimation.py
+++ src/tests/test_trace_estimation.py
@@ -6,7 +6,7 @@ from scipy.linalg import logm
 
 import pytest
 
-from trace_estimation import fA_b
+from trace_estimation import fA_b, hutchinson
 
 
 def test_Ab(dense_symmetric_matrix, random_seed):
@@ -34,8 +34,21 @@ def test_AAb(dense_symmetric_matrix, random_seed):
 
 
 def test_logAb(positive_definite_matrix, random_seed):
+    """
+    Test to see whether the lanczos approximation to log(A)b is correct.
+    """
     M = positive_definite_matrix
     rng = np.random.default_rng(random_seed)
     b = rng.standard_normal(M.shape[0])
 
     np.testing.assert_allclose(fA_b(np.log, M, b), logm(M) @ b, rtol=1e-5, atol=1e-5)
+
+
+def test_trA(sparse_symmetric_matrix, random_seed):
+    """
+    Test to see whether the tr(A) approximation is correct.
+    """
+    M = sparse_symmetric_matrix
+    trM = M.trace()
+    trM_aprx, samps = hutchinson(M, lambda x: x, n=1500, seed=random_seed)
+    np.testing.assert_allclose(trM_aprx, trM, rtol=1e-1, atol=1e-1)
--- src/trace_estimation.py
+++ src/trace_estimation.py
@@ -45,11 +45,45 @@ def fA_b(
     return V @ (U @ (f(L) * (U.T @ (ebeta))))
 
 
-# def hutchinson(A, f, n_hutch, k_lanczos, rng, reorth=False, verbose=False):
-#    n = A.shape[0]
-#    X = rng.standard_normal((n, n_hutch))
-#    samps = np.zeros(n_hutch)
-#    for i in range(n_hutch):
-#        fA_Xi = fA_b(f, A, X[:, i], k_lanczos, reorth=reorth)
-#        samps[i] = np.inner(X[:, i], fA_Xi)
-#    return samps.mean(), samps
+def hutchinson(
+    A: linear_operator,
+    f: Callable[[np.ndarray], np.ndarray],
+    n: int = 30,
+    seed=None,
+    **lanczos_opts,
+) -> tuple[float, np.ndarray]:
+    """Approximate tr[f(A)] using Hutchinson + Lanczos.
+
+    Using the Hutchinson algorithm to approximate the trace and the Lanczos algrorithm
+    to approximate f(A)b, we can approximate tr[f(A)]. See the following paper for
+    details:
+
+    Ubaru, S., Chen, J., & Saad, Y. (2017). Fast Estimation of $tr(f(A))$ via Stochastic
+    Lanczos Quadrature. In SIAM Journal on Matrix Analysis and Applications (Vol. 38,
+    Issue 4, pp. 1075–1099). Society for Industrial & Applied Mathematics (SIAM).
+    https://doi.org/10.1137/16m1104974
+
+    Args:
+        A (linear_operator): Symmetric matrix to apply f to.
+        f (Callable[[np.ndarray], np.ndarray]): Function to apply to A.
+
+    Keyword Args:
+        n (int): Number of samples to use in the Hutchinson algorithm. Defaults to 30.
+        seed: Random seed for the Hutchinson algorithm. Will be passed to
+            np.random.default_rng, so can be an int, array_like, or BitGenerator.
+            Defaults to None.
+        reorth (bool): Whether to reorthogonalize the Lanczos vectors. This can be
+            quite expensive, but may be necessary for stability. Defaults to False.
+        k (int): Number of Lanczos iterations. Defaults to 25.
+
+    Returns:
+        tuple[float, np.ndarray]: Tuple of the estimate of tr[f(A)] and the samples
+            used to compute the estimate.
+    """
+    rng = np.random.default_rng(seed)
+    # X = rng.standard_normal((A.shape[0], n))
+    X = 2 * (rng.binomial(1, 0.5, size=(A.shape[0], n)) - 0.5)
+    samps = np.zeros(n)
+    for i in range(n):
+        samps[i] = np.inner(X[:, i], fA_b(f, A, X[:, i], **lanczos_opts))
+    return samps.mean(), samps
